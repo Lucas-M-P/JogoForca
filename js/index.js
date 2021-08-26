15c3a851a4b8f38bf50156027e7d69d0219807be
Lucas Miguel Pimenta <lucas_m_p@hotmail.com>
2021-08-25 21:48:04 -0300

    added main menu modal

--- js/index.js
+++ js/index.js
@@ -1,49 +1,65 @@
 var divSecretWord = document.querySelector(".secretWord");
 var buttons = document.querySelectorAll(".letter");
 var image = document.querySelector("#image");
+var modal = document.querySelector("#myModal");
+var button = document.querySelector(".playButton");
 var errors = 0;
 
-var secretWord = "LUCAS";
+var secretWord;
 
-for (let index = 0; index < secretWord.length; index++) {
-    divSecretWord.textContent += "_";
-}
-
-buttons.forEach(element => {
-    element.addEventListener("click", verifyLetter)
+window.addEventListener("load", () => {
+    modal.style.display = "block";
 });
 
-function verifyLetter(event){
-    if(secretWord.toUpperCase().includes(event.target.textContent)){
-        correctLetter(event.target.textContent, event);
-    }
-    else{
-        wrongLetter(event);
-    }
-}
+button.addEventListener("click", startGame);
 
-function correctLetter(letter, event){
-    var arrayDivSecretWord = divSecretWord.textContent.split("");
-    divSecretWord.textContent = "";
+function startGame(event){
+    event.preventDefault();
+
+    secretWord = "GALINHA";
 
     for (let index = 0; index < secretWord.length; index++) {
-        if(secretWord[index] == letter){
-            arrayDivSecretWord[index] = letter;
-            divSecretWord.textContent += arrayDivSecretWord[index];
+        divSecretWord.textContent += "_";
+    }
+    
+    buttons.forEach(element => {
+        element.addEventListener("click", verifyLetter)
+    });
+    
+    function verifyLetter(event){
+        if(secretWord.toUpperCase().includes(event.target.textContent)){
+            correctLetter(event.target.textContent, event);
         }
         else{
-            divSecretWord.textContent += arrayDivSecretWord[index];
+            wrongLetter(event);
         }
     }
+    
+    function correctLetter(letter, event){
+        var arrayDivSecretWord = divSecretWord.textContent.split("");
+        divSecretWord.textContent = "";
+    
+        for (let index = 0; index < secretWord.length; index++) {
+            if(secretWord[index] == letter){
+                arrayDivSecretWord[index] = letter;
+                divSecretWord.textContent += arrayDivSecretWord[index];
+            }
+            else{
+                divSecretWord.textContent += arrayDivSecretWord[index];
+            }
+        }
+    
+        event.target.style.backgroundColor = "lightgreen";
+        event.target.removeEventListener("click", verifyLetter);
+    }
+    
+    function wrongLetter(event){
+        event.target.style.backgroundColor = "lightcoral";
+        event.target.removeEventListener("click", verifyLetter);
+    
+        errors++;
+        image.src = "images/Forca" + errors + ".png";
+    }
 
-    event.target.style.backgroundColor = "lightgreen";
-    event.target.removeEventListener("click", verifyLetter);
-}
-
-function wrongLetter(event){
-    event.target.style.backgroundColor = "lightcoral";
-    event.target.removeEventListener("click", verifyLetter);
-
-    errors++;
-    image.src = "images/Forca" + errors + ".png";
+    modal.style.display = "none";
 }
